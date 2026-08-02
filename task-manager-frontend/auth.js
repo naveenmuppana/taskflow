9c128255f3114cdf8f263752390b3e466d93f2ef
naveen muppana <naveenmuppana123@gmail.com>
2026-08-02 12:41:05 +0530

    changes

--- task-manager-frontend/auth.js
+++ task-manager-frontend/auth.js
@@ -14,7 +14,7 @@ function toggleTheme() {
     }
 }
 
-const API_URL = 'http://localhost:8000/api/v1';
+const API_URL = 'https://taskflow-cr3c.onrender.com/api/v1';
 
 // State
 let currentAuthTab = 'login';
